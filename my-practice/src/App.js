bb9e88afa6034e8a974684a435e4455a615b079c
Saad Hmed <saad.ahmed2696@gmail.com>
2020-09-29 12:17:26 +0500

    stateful functiona and class components

---
+++ my-practice/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Greet from './components/Greet'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+   <Greet/>
+      </div>
+    );
+  }
+}
+export default App; 
---
+++ my-practice/src/components/Greet.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+ const Greet=()=><h1>Hello saad ahmed</h1>
+
+export default Greet;
